beab63ef065002113a10a1cba032611aa3a8692b
ujjwaldubey1 <ujjwal07dubey@gmail.com>
2025-10-29 23:38:45 +0530

    Add: Public privete key genrating feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ethers } from 'ethers'
+import { generateMnemonic } from 'bip39';
+
 
 const App = () => {
+  const [publicKey , setPublicKey] = useState('');
+  const [privateKey , setPrivateKey] = useState('');
+  const [mnemonics, setMnemonics] = useState('');
+   const handleGenratePublickPrivateKey = () =>{
+        const newWallet = ethers.Wallet.createRandom();
+
+        setPublicKey(newWallet.publicKey);
+        setPrivateKey(newWallet.privateKey);
+        
+
+   }
+
+   const handleMnemonics = () =>{
+
+   }
+
   return (
     <div>
-      <h1 className='bg-red-500'>Web based wallet </h1>
+      <button className='bg-green-700 rounded-md' onClick={handleGenratePublickPrivateKey}>Create Wallet </button>
 
-      <button className='bg-green-700 rounded-md '>Create Wallet </button>
+      <div>
+        <p > Public key : {publicKey}</p>
+        <p>Private key : {privateKey}</p>
+      </div>
 
     </div>
   )
