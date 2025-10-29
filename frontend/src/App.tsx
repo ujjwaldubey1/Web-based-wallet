41dffc9695c24f501999a5ab3dd9b0e1662b20f9
ujjwaldubey1 <ujjwal07dubey@gmail.com>
2025-10-29 22:04:40 +0530

    Initialsed the project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-red-500'>hello world</h1>
+      <h1 className='bg-red-500'>Web based wallet </h1>
+
+      <button className='bg-green-700 rounded-md '>Create Wallet </button>
+
     </div>
   )
 }
